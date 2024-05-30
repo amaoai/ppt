968ceb0e1b172479afdf2a710dc515729c744c7d
am1234567891 <andrew.j.mao@gmail.com>
2024-05-30 09:48:10 +0800

    fixed errors in logic

--- test.py
+++ test.py
@@ -33,16 +33,17 @@ def clean_sheet(df, sheet_name):
     return df_6
 
 
-def create_picture(df, target_rows_lb, target_rows_db, name):
+def create_picture(df_in, target_rows_lb, target_rows_db, name):
     colors = []
-
-    for row in range(0, df.shape[0]):
+    print(target_rows_lb, target_rows_db)
+    for row in range(0, df_in.shape[0]):
         if row in target_rows_lb:
             colors.append(["#c1c9db","#c1c9db","#c1c9db","#c1c9db","#c1c9db"])
         elif row in target_rows_db:
             colors.append(["#8191b5","#8191b5","#8191b5","#8191b5","#8191b5"])
         else:
             colors.append(["w","w","w","w","w"])
+    print(colors)
 
     fig, ax = plt.subplots()
 
@@ -51,7 +52,7 @@ def create_picture(df, target_rows_lb, target_rows_db, name):
 
     plt.rcParams['text.color'] = 'black'
 
-    table = ax.table(cellText=df.values, cellColours=colors,  loc='center')
+    table = ax.table(cellText=df_in.values, cellColours=colors,  loc='center')
     table.auto_set_font_size(False)
     table.set_fontsize(11)
     table.scale(1.5,2)
@@ -72,6 +73,7 @@ def create_slide_1(df_6):
     # picture 1
     name_list1 = df_6.index[df_6[1] == '資産合計'].tolist()
     df_p1 = df_6.iloc[:name_list1[0]+1]
+    df_p1.reset_index(inplace=True, drop=True)
     target_rows_lb1 = []
     target_rows_db1 = [1]
     target_rows_lb1 = target_rows_lb1 + df_p1.index[df_p1[1] == '流動資産'].tolist()
@@ -82,6 +84,7 @@ def create_slide_1(df_6):
     # picture 2
     name_list2 = df_6.index[df_6[1] == '負債合計'].tolist() 
     df_p2 = df_6.iloc[name_list1[0]+1:name_list2[0]+1]
+    df_p2.reset_index(inplace=True, drop=True)
     target_rows_lb2 = []
     target_rows_db2 = [1]
     target_rows_lb2 = target_rows_lb2 + df_p2.index[df_p2[1] == '流動負債'].tolist()
@@ -92,74 +95,7 @@ def create_slide_1(df_6):
     # picture 3
     name_list3 = df_6.index[df_6[1] == '負債・純資産合計'].tolist() 
     df_p3 = df_6.iloc[name_list2[0]+1:name_list3[0]+1]
-    target_rows_lb3 = []
-    target_rows_db3 = []
-    target_rows_lb3 = target_rows_lb3 + df_p3.index[df_p3[1] == '株主資本'].tolist()
-    target_rows_db3 = target_rows_lb3 + df_p3.index[df_p3[1] == '純資産合計'].tolist()
-    target_rows_db3 = target_rows_db3 + df_p3.index[df_p3[1] == '負債・純資産合計'].tolist()
-    pic_p3 = create_picture(df_p3, target_rows_lb3, target_rows_db3, 's1p3.png')
-
-    # scale picture 1
-    img1 = Image.open(pic_p1)
-    original_width1, original_height1 = img1.size
-    new_height1 = 6
-    scaling_factor1 = new_height1/(original_height1/1000)
-    new_width1 = scaling_factor1*original_width1/1000
-
-    # scale picture 2
-    img2 = Image.open(pic_p2)
-    original_width2, original_height2 = img2.size
-    new_height2 = 3
-    scaling_factor2 = new_height2/(original_height2/1000)
-    new_width2 = scaling_factor2*original_width2/1000
-
-    # scale picture 3
-    img3 = Image.open(pic_p3)
-    original_width3, original_height3 = img3.size
-    new_height3 = 2
-    scaling_factor3 = new_height3/(original_height3/1000)
-    new_width3 = scaling_factor3*original_width3/1000
-
-    # presentation initialize
-    slide_layout1 = prs.slide_layouts[5]  # Using a title-only layout
-    slide1 = prs.slides.add_slide(slide_layout1)
-    title = slide1.shapes.title
-    title.text = "貸借対照表（3期比較）"
-    if title.text_frame:
-        for paragraph in title.text_frame.paragraphs:
-            for run in paragraph.runs:
-                run.font.size = Pt(20)
-    pic1 = slide1.shapes.add_picture(pic_p1, Inches(0.5), Inches(1.3), width=Inches(new_width1), height=Inches(new_height1))
-    pic2 = slide1.shapes.add_picture(pic_p2, Inches(4.5), Inches(1.3), width=Inches(new_width2), height=Inches(new_height2))
-    pic3 = slide1.shapes.add_picture(pic_p3, Inches(4.5), Inches(4.3), width=Inches(new_width3), height=Inches(new_height3))
-
-
-def create_slide_1(df_6):
-    global prs
-
-    # picture 1
-    name_list1 = df_6.index[df_6[1] == '資産合計'].tolist()
-    df_p1 = df_6.iloc[:name_list1[0]+1]
-    target_rows_lb1 = []
-    target_rows_db1 = [1]
-    target_rows_lb1 = target_rows_lb1 + df_p1.index[df_p1[1] == '流動資産'].tolist()
-    target_rows_lb1 = target_rows_lb1 + df_p1.index[df_p1[1] == '固定資産'].tolist()
-    target_rows_db1 = target_rows_db1 + df_p1.index[df_p1[1] == '資産合計'].tolist()
-    pic_p1 = create_picture(df_p1, target_rows_lb1, target_rows_db1, 's1p1.png')
-
-    # picture 2
-    name_list2 = df_6.index[df_6[1] == '負債合計'].tolist() 
-    df_p2 = df_6.iloc[name_list1[0]+1:name_list2[0]+1]
-    target_rows_lb2 = []
-    target_rows_db2 = [1]
-    target_rows_lb2 = target_rows_lb2 + df_p2.index[df_p2[1] == '流動負債'].tolist()
-    target_rows_lb2 = target_rows_lb2 + df_p2.index[df_p2[1] == '固定負債'].tolist()
-    target_rows_db2 = target_rows_db2 + df_p2.index[df_p2[1] == '負債合計'].tolist()
-    pic_p2 = create_picture(df_p2, target_rows_lb2, target_rows_db2, 's1p2.png')
-
-    # picture 3
-    name_list3 = df_6.index[df_6[1] == '負債・純資産合計'].tolist() 
-    df_p3 = df_6.iloc[name_list2[0]+1:name_list3[0]+1]
+    df_p3.reset_index(inplace=True, drop=True)
     target_rows_lb3 = []
     target_rows_db3 = []
     target_rows_lb3 = target_rows_lb3 + df_p3.index[df_p3[1] == '株主資本'].tolist()
@@ -208,6 +144,7 @@ def create_slide_2(df_6):
     # picture 1
     name_list1 = df_6.index[df_6[1] == '当期純利益'].tolist()
     df_p1 = df_6.iloc[:name_list1[0]+1]
+    df_p1.reset_index(inplace=True, drop=True)
     target_rows_lb1 = []
     target_rows_db1 = [1]
     target_rows_lb1 = target_rows_lb1 + df_p1.index[df_p1[1] == '売上高'].tolist()
@@ -229,8 +166,10 @@ def create_slide_2(df_6):
     name_list2_1 = df_6.index[df_6[1] == '製造原価（3ヵ年推移）'].tolist() 
     name_list2_2 = df_6.index[df_6[1] == '合計'].tolist()
     df_p2 = df_6.iloc[name_list2_1[0]:name_list2_2[0]+1]
+    df_p2.reset_index(inplace=True, drop=True)
     target_rows_lb2 = [2]
-    target_rows_db2 = [1, name_list2_2]
+    last = df_p2.index[df_p2[1] == '合計'].tolist()
+    target_rows_db2 = [1, last[0]]
     pic_p2 = create_picture(df_p2, target_rows_lb2, target_rows_db2, 's1p2.png')
 
     # scale picture 1
@@ -267,7 +206,9 @@ def create_slide_3(df_6):
     name_list1_1 = df_6.index[df_6[1] == '製造原価（3ヵ年推移）'].tolist()
     name_list1_2 = df_6.index[df_6[1] == '合計'].tolist()
     df_p1 = df_6.iloc[name_list1_1[0]+1:name_list1_2[0]+1]
-    target_rows_lb1 = [name_list1_2]
+    df_p1.reset_index(inplace=True, drop=True)
+    last = df_p1.index[df_p1[1] == '合計'].tolist()
+    target_rows_lb1 = [last[0]]
     target_rows_db1 = [1, 2]
     pic_p1 = create_picture(df_p1, target_rows_lb1, target_rows_db1, 's3p1.png')
 
